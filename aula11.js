66c96362a31d68b0083f81148eeddd36a54a5cab
“Rafa <indhuge@gmail.com>
2024-09-21 09:56:41 -0300

    feat: lidando com operadores básicos no JS

---
+++ aula11.js
@@ -0,0 +1,12 @@
+// aula de operadores
+// soma +
+// divisao /
+// multiplicação *
+// subtração 
+
+function soma (valor1, valor2)
+{
+    return valor1 + valor2;
+}
+
+console.log('A soma de ', soma(2,5));
