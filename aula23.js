667993229d1cc22db15ee653a8e2e5c1c7a566ce
“Rafa <indhuge@gmail.com>
2024-09-21 17:59:08 -0300

    feat: recursividade

---
+++ aula23.js
@@ -0,0 +1,10 @@
+// praticando recursividade
+
+const fatorial = (valor) =>{
+    if ((valor === 0)||(valor === 1))
+        return 1;
+
+    return valor*fatorial(valor-1);
+}
+
+console.log('Fatorial ', fatorial(6));
