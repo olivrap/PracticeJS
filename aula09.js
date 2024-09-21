e2fa6505788bc2bcbac49e562035ac029cfa0de3
“Rafa <indhuge@gmail.com>
2024-09-21 00:42:12 -0300

    feat: lidando com funções e retornos

---
+++ aula09.js
@@ -0,0 +1,10 @@
+//lidando com funções e somas
+function soma (item1, item2){
+    return item1+item2;
+}
+
+function mult(item1, item2){
+    return item1*item2;
+}
+console.log('A soma dos dois itens é', soma(45,23));
+console.log('A multiplicação dos dois itens é', mult(45,23));
