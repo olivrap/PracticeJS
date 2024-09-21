3cdfe6366ca0cd48f088642d8d0cefcb9a38c467
“Rafa <indhuge@gmail.com>
2024-09-21 16:57:16 -0300

    feat: estudando escopo lexico

---
+++ aula20.js
@@ -0,0 +1,4 @@
+// escopo léxico
+// variáveis sao vistas dentro dos menores escopos para os maiores
+// quando nao encontradas, o JS avisa que a variavel é indefinida
+// não há código para representar isso
