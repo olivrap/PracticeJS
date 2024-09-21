6fcaba29f2f188248bd022df7039ae305f0864ad
“Rafa <indhuge@gmail.com>
2024-09-20 22:40:11 -0300

    feat: aula04 sobre o for

---
+++ aula04.js
@@ -0,0 +1,12 @@
+//vamos praticar o for
+
+const passo = 15;
+
+for (let i = 0; i<=passo; i++)
+{
+    console.log('passo ',i);
+}
+// o for precisa de 3 parametros
+// inicialização
+// condição de parada
+// incremento
