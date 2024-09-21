92c78c5971f2ac2c5083f7329dc2b3055d9e4fe7
“Rafa <indhuge@gmail.com>
2024-09-21 09:59:43 -0300

    feat: melhora de código

--- aula11.js
+++ aula11.js
@@ -4,9 +4,18 @@
 // multiplicação *
 // subtração 
 
+// % resto
+// ** potencia (elevado)
+
 function soma (valor1, valor2)
 {
     return valor1 + valor2;
 }
 
+function pot (valor1, valor2)
+{
+    return valor1**valor2;
+}
+
 console.log('A soma de ', soma(2,5));
+console.log('A potencia eh ', pot(2,3));
