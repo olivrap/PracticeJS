e2a4db99d69a8a3de009896781984c9964a6e0af
“Rafa <indhuge@gmail.com>
2024-09-20 22:27:23 -0300

    feat: aula03 - aprendendo ifs e comparacoes

---
+++ aula03.js
@@ -0,0 +1,15 @@
+// aprendendo IF
+const idade = 20;
+let podeEntrar =  false;
+
+if (idade > 20)
+{
+    podeEntrar = true;
+} else{
+    console.log('Nao entrou ... ')
+}
+
+console.log('Usuario pode entrar ', podeEntrar);
+
+// os 3 sinais de igual fazem uma comparação
+// mais precisa e nao fazem conversão de tipo
