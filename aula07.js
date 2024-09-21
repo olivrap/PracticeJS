59fcac6d128f032029224f47c8825339b0c5cea1
“Rafa <indhuge@gmail.com>
2024-09-21 00:10:03 -0300

    feat: aula de como utilizar arrays

---
+++ aula07.js
@@ -0,0 +1,12 @@
+// praticando com arrays
+
+const idadeRafa = 18;
+const idadeJu = 19;
+const idadeMaria = 23;
+
+const idades = [idadeRafa, idadeJu, idadeMaria];
+
+for(let i = 0; i<idades.length; i++)
+{
+    console.log('Idades ',i,' : ', idades[i]);
+}
